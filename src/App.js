9bc506baa675b65e4937b11609854524b622e3a7
ColonelBucket8 <freedomterran89@gmail.com>
2022-04-24 08:19:04 +0800

    added uuid to monsters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
 class App extends Component {
@@ -8,18 +9,21 @@ class App extends Component {
     this.state = {
       monsters: [
         {
-            name: "Linda",
+          name: "Linda",
+          id: uuidv4(),
         },
         {
           name: "Frank",
+          id: uuidv4(),
         },
         {
           name: "Jacky",
+          id: uuidv4(),
         },
-          {
+        {
           name: "Andrei",
+          id: uuidv4(),
         },
-
       ],
     };
   }
@@ -28,7 +32,9 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.monsters.map((monster) => (
-          <h1>{monster.name}</h1>
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
         ))}
       </div>
     );
