4781718c3c78949ee67fafdb0fa514f18079f6f7
Aimi Fadhil <59568628+ColonelBucket8@users.noreply.github.com>
2022-04-25 23:06:59 +0800

    added search component to filter monsters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from "react";
-import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
 class App extends Component {
@@ -7,31 +6,44 @@ class App extends Component {
     super(props);
 
     this.state = {
-      monsters: [
-        {
-          name: "Linda",
-          id: uuidv4(),
-        },
-        {
-          name: "Frank",
-          id: uuidv4(),
-        },
-        {
-          name: "Jacky",
-          id: uuidv4(),
-        },
-        {
-          name: "Andrei",
-          id: uuidv4(),
-        },
-      ],
+      monsters: [],
+      searchField: "",
     };
   }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+  onSearchChange = (evt) => {
+    const searchString = evt.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField: searchString };
+    });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search Monsters"
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => (
           <div key={monster.id}>
             <h1>{monster.name}</h1>
           </div>
